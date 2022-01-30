09e2a74d68a9d9c678f61384f11957b1044c949e
Luc Engelen <16940687+ljpengelen@users.noreply.github.com>
2022-01-30 20:12:43 +0100

    Add first assignments

---
+++ src/cfb/assignments.cljs
@@ -0,0 +1,83 @@
+(ns cfb.assignments)
+
+(defn random-numbers
+  ([] (random-numbers 2000))
+  ([m] (repeatedly 20 #(rand-int m))))
+
+(defn reverse-list []
+  (let [x (random-numbers)]
+    {:description
+     [:<>
+      [:p
+       "Welcome to Clojure for beginners! "
+       "You'll be given a number of questions, one by one, and a code editor for writing your solution for each of them. "
+       "There's a button and a shortcut you can use to check your answer at any time. "]
+      [:p
+       "The var " [:code "x"] " below is bound to a list of random numbers. "
+       "Reverse this list by replacing the underscore."]
+      [:p "You can use " [:a {:href "https://clojuredocs.org/"} "ClojureDocs"] " to search for useful functions."]]
+     :expressions (str
+                   "(def x '" x ")\n\n"
+                   "(_ x)")
+     :solution (reverse x)}))
+
+(defn core-link [f]
+  [:a {:href (str "https://clojuredocs.org/clojure.core/" f)} f])
+
+(defn find-max []
+  (let [x (random-numbers)]
+    {:description
+     [:<>
+      [:p
+       "The var " [:code "x"] " below is bound to a list of random numbers. "
+       "Find the largest number by replacing the underscore."]
+      [:p
+       "The function " (core-link "max") " takes a variable number of arguments. "
+       "Applying max to a single argument will just return that argument."]]
+     :expressions (str
+                   "(def x '" x ")\n\n"
+                   "(_ max x)")
+     :solution (apply max x)}))
+
+(defn find-second-largest []
+  (let [x (random-numbers)]
+    {:description
+     [:<>
+      [:p
+       "The var " [:code "x"] " below is bound to a list of random numbers. "
+       "Find the second largest number."]
+      [:p
+       "Clojure's " (core-link "sort") " function returns a sorted sequence. "
+       "Clojure offers a number of functions to take certain elements or subsets from collections and sequences, "
+       "such as " (core-link "first") ", " (core-link "second") ", " (core-link "rest") ", and " (core-link "next") "."]]
+     :expressions (str
+                   "(def x '" x ")\n\n"
+                   "(_ (_ (sort > x)))")
+     :solution (->> x
+                    (sort >)
+                    next
+                    first)}))
+
+(defn second-smallest-even []
+  (let [x (random-numbers)]
+    {:description
+     [:<>
+      [:p
+       "The var " [:code "x"] " below is bound to a list of random numbers. "
+       "Find the second smallest even number."]]
+     :expressions (str
+                   "(def x '" x ")\n\n"
+                   "(->> x\n"
+                   "     (filter _)\n"
+                   "     _\n"
+                   "     _)")
+     :solution (->> x
+                    (filter even?)
+                    sort
+                    second)}))
+
+(def assignments
+  [(reverse-list)
+   (find-max)
+   (find-second-largest)
+   (second-smallest-even)])
--- src/cfb/core.cljs
+++ src/cfb/core.cljs
@@ -1,83 +1,67 @@
 (ns cfb.core
-  (:require ["@codemirror/legacy-modes/mode/clojure" :refer [clojure]]
-            ["@codemirror/stream-parser" :refer [StreamLanguage]]
-            ["@uiw/react-codemirror" :default CodeMirror]
+  (:require [cfb.assignments :as a]
+            [cfb.views :as v]
             [reagent.core :as r]
             [reagent.dom :as d]
             [sci.core :as sci]))
 
-(defonce assignments
-  [{:description "Blaat"
-    :expressions "(def x [1 2 3 4])"
-    :solution 10}
-   {:description "Aap"
-    :expressions "(def x [1 2 3 4])"
-    :solution 24}])
-(def number-of-assignments (count assignments))
+(def number-of-assignments (count a/assignments))
 
 (defonce index (r/atom 0))
-(defonce description (r/atom (:description (assignments @index))))
-(defonce expressions (r/atom (:expressions (assignments @index))))
+(defonce description (r/atom (:description (a/assignments @index))))
+(defonce expressions (r/atom (:expressions (a/assignments @index))))
 (defonce solution (r/atom nil))
 
 (defonce answer (r/atom nil))
 (defonce show-result? (r/atom false))
 (defonce error-message (r/atom nil))
 
+(defn clear! []
+  (reset! answer nil)
+  (reset! show-result? false)
+  (reset! error-message nil))
+
 (defn show-assignment! [index]
-  (let [assignment (assignments index)]
+  (let [assignment (a/assignments index)]
+    (clear!)
     (reset! description (:description assignment))
     (reset! expressions (:expressions assignment))
     (reset! solution (:solution assignment))))
 
-(show-assignment! @index)
-
-(defn editor [expressions on-change]
-  [(r/adapt-react-class CodeMirror) {:extensions [(.define StreamLanguage clojure)]
-                                     :height "10rem"
-                                     :on-change (fn [value] (on-change value))
-                                     :theme "dark"
-                                     :value expressions
-                                     :width "40rem"}])
-
-(defn result [answer solution show-result?]
-  (js/console.log answer solution)
-  (when show-result?
-    [:div
-     [:span (if (= answer solution) "YES!" "NO!")]
-     [:pre [:code (pr-str answer)]]]))
-
-(defn error [error-message]
-  [:div error-message])
-
 (defn evaluate! [expressions]
+  (clear!)
   (try
     (reset! answer (sci/eval-string expressions))
     (reset! show-result? true)
     (catch :default e (reset! error-message (.-message e)))))
 
-(defn next-assignment! []
+(defn show-next-assignment! []
   (swap! index inc)
   (show-assignment! @index))
 
-(defn previous-assignment! []
+(defn show-previous-assignment! []
   (swap! index dec)
   (show-assignment! @index))
 
+(defn update-expressions! [new-expressions]
+  (reset! expressions new-expressions))
+
 (defn app []
   [:div.app
-   [:div @description]
-   [editor @expressions (fn [new-expressions] (reset! expressions new-expressions))]
-   [:button {:on-click #(evaluate! @expressions)} "Check answer"]
-   [result @answer @solution @show-result?]
-   [error @error-message]
-   [:button {:on-click previous-assignment!
-             :disabled (<= @index 0)} "Previous question"]
-   [:button {:on-click next-assignment!
-             :disabled (>= (inc @index) number-of-assignments)} "Next question"]])
+   [:h1 "Clojure for beginners"]
+   [:div.description @description]
+   [v/editor @expressions update-expressions! #(evaluate! @expressions)]
+   [v/check-answer-button #(evaluate! @expressions)]
+   [v/result @answer @solution @show-result?]
+   [v/error @error-message]
+   [:button {:on-click show-previous-assignment!
+             :disabled (<= @index 0)} "Previous assignment"]
+   [:button {:on-click show-next-assignment!
+             :disabled (>= (inc @index) number-of-assignments)} "Next assignment"]])
 
 (defn mount-root []
   (d/render [app] (.getElementById js/document "app")))
 
 (defn ^:export init! []
+  (show-assignment! @index)
   (mount-root))
